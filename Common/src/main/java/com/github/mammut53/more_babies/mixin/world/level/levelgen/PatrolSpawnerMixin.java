08aac5422d51d7bc1640132b0794031f6fc7b411
mammut53 <git@mammut53.me>
2022-08-01 14:02:06 +0200

    Added baby pillager patrol spawning

---
+++ Common/src/main/java/com/github/mammut53/more_babies/mixin/world/level/levelgen/PatrolSpawnerMixin.java
@@ -0,0 +1,37 @@
+package com.github.mammut53.more_babies.mixin.world.level.levelgen;
+
+import com.github.mammut53.more_babies.MoreBabiesCommon;
+import com.github.mammut53.more_babies.MoreBabiesConstants;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.levelgen.PatrolSpawner;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(PatrolSpawner.class)
+public abstract class PatrolSpawnerMixin {
+
+    @Redirect(
+            method = "spawnPatrolMember",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/EntityType;create(Lnet/minecraft/world/level/Level;)Lnet/minecraft/world/entity/Entity;"
+            )
+    )
+    private Entity createPillager(EntityType<Entity> entityType, Level level) {
+        if (!(level.random.nextFloat() < MoreBabiesConstants.BABY_SPAWN_CHANCE)) {
+            return entityType.create(level);
+        }
+
+        EntityType<? extends Mob> babyType = MoreBabiesCommon.PARENT_BABY_RELATION.get(entityType);
+        if (babyType == null) {
+            return entityType.create(level);
+        }
+
+        return babyType.create(level);
+    }
+
+}
