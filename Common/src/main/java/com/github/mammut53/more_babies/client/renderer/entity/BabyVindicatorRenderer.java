0dd7d5d5e678d45c05c911ceeacdca5f48acf29d
mammut53 <git@mammut53.me>
2022-08-01 19:28:51 +0200

    Fixed baby vindicator weapon alignment

--- Common/src/main/java/com/github/mammut53/more_babies/client/renderer/entity/BabyVindicatorRenderer.java
+++ Common/src/main/java/com/github/mammut53/more_babies/client/renderer/entity/BabyVindicatorRenderer.java
@@ -1,14 +1,34 @@
 package com.github.mammut53.more_babies.client.renderer.entity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.model.IllagerModel;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.VindicatorRenderer;
+import net.minecraft.client.renderer.entity.IllagerRenderer;
+import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.monster.Vindicator;
 
-public class BabyVindicatorRenderer extends VindicatorRenderer {
+public class BabyVindicatorRenderer extends IllagerRenderer<Vindicator> {
+
+    private static final ResourceLocation VINDICATOR = new ResourceLocation("textures/entity/illager/vindicator.png");
 
     public BabyVindicatorRenderer(EntityRendererProvider.Context context) {
-        super(context);
+        super(context, new IllagerModel<>(context.bakeLayer(ModelLayers.VINDICATOR)), 0.5F);
+        this.addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()) {
+            public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, Vindicator vindicator, float f, float g, float h, float j, float k, float l) {
+                if (!vindicator.isAggressive()) return;
+
+                poseStack.scale(2F, 2F, 2F);
+                poseStack.translate(0, -0.75, 0);
+                super.render(poseStack, multiBufferSource, i, vindicator, f, g, h, j, k, l);
+            }
+        });
+    }
+
+    public ResourceLocation getTextureLocation(Vindicator vindicator) {
+        return VINDICATOR;
     }
 
     @Override
