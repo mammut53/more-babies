09ade3c4087d8ca04ff8ba2f76c1559b1e03da0c
mammut53 <git@mammut53.me>
2022-08-02 03:03:14 +0200

    Added baby blaze spawner in nether fortresses

---
+++ Common/src/main/java/com/github/mammut53/more_babies/mixin/world/level/levelgen/structure/MonsterThroneMixin.java
@@ -0,0 +1,43 @@
+package com.github.mammut53.more_babies.mixin.world.level.levelgen.structure;
+
+import com.github.mammut53.more_babies.MoreBabiesCommon;
+import com.github.mammut53.more_babies.MoreBabiesConstants;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.level.levelgen.structure.structures.NetherFortressPieces;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(NetherFortressPieces.MonsterThrone.class)
+public abstract class MonsterThroneMixin {
+
+    @Inject(
+            method = "postProcess",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/BaseSpawner;setEntityId(Lnet/minecraft/world/entity/EntityType;)V",
+                    shift = At.Shift.AFTER
+            ),
+            locals = LocalCapture.CAPTURE_FAILSOFT
+    )
+    private void setEntityId(WorldGenLevel worldGenLevel, StructureManager structureManager, ChunkGenerator chunkGenerator, RandomSource randomSource, BoundingBox boundingBox, ChunkPos chunkPos, BlockPos blockPos, CallbackInfo ci, BlockState $$7, BlockState $$8, BlockPos $$9, BlockEntity blockEntity) {
+        if (!(randomSource.nextFloat() < MoreBabiesConstants.BABY_SPAWN_CHANCE)) {
+            return;
+        }
+
+        ((SpawnerBlockEntity)blockEntity).getSpawner().setEntityId(MoreBabiesCommon.PARENT_BABY_RELATION.get(EntityType.BLAZE));
+    }
+
+}
