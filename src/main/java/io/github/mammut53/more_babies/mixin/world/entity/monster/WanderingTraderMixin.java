92de5e29d499e3ee2838a87ec8b35a6f62ac7af1
mammut53 <git@mammut53.me>
2026-03-27 20:25:04 +0100

    fix wandering trader baby dimensions

--- src/main/java/io/github/mammut53/more_babies/mixin/world/entity/monster/WanderingTraderMixin.java
+++ src/main/java/io/github/mammut53/more_babies/mixin/world/entity/monster/WanderingTraderMixin.java
@@ -1,16 +1,21 @@
 package io.github.mammut53.more_babies.mixin.world.entity.monster;
 
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.npc.villager.AbstractVillager;
 import net.minecraft.world.entity.npc.wanderingtrader.WanderingTrader;
 import net.minecraft.world.item.component.Consumable;
 import net.minecraft.world.level.Level;
+import org.jspecify.annotations.NonNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(WanderingTrader.class)
 public abstract class WanderingTraderMixin extends AbstractVillager implements Consumable.OverrideConsumeSound {
+
+    @Unique
+    private static final EntityDimensions more_babies$BABY_DIMENSIONS = EntityDimensions.scalable(0.49F, 0.99F).withEyeHeight(0.63F);
 
     protected WanderingTraderMixin(final EntityType<? extends AbstractVillager> type, final Level level) {
         super(type, level);
@@ -23,8 +28,13 @@ public abstract class WanderingTraderMixin extends AbstractVillager implements C
                     target = "Lnet/minecraft/world/entity/npc/wanderingtrader/WanderingTrader;setAge(I)V"
             )
     )
-    private int injectReadAdditionalSaveDataSetBaby(final int par1) {
+    private int injectReadAdditionalSaveDataSetBaby(final int input) {
         return this.getAge();
+    }
+
+    @Override
+    public @NonNull EntityDimensions getDefaultDimensions(final @NonNull Pose pose) {
+        return this.isBaby() ? more_babies$BABY_DIMENSIONS : super.getDefaultDimensions(pose);
     }
 
 }
